be650e1275536eabed115f832bd7d1709748e2f9
jiangshan <1358470785@qq.com>
2026-04-24 15:35:27 +0800

    测试pull

--- gfr-web/accountingSystemCorrespondence.js
+++ gfr-web/accountingSystemCorrespondence.js
@@ -1,6 +1,7 @@
 var uploader = null //uploader上传对象
 var fileNum = 0; //已上传附件数 修改内容 测试qader修改
 var fileName = null
+//测试pull
 var reportClassInfo = {
     report_class: '',
     report_class_name: ''
